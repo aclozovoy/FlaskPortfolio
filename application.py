903c95506672a6012e0a2bfe66f9d3c77cd4cb2d
aclozovoy <aclozovoy@gmail.com>
2022-09-29 15:48:43 -0700

    test

--- application.py
+++ application.py
@@ -1,5 +1,5 @@
 from website import create_app
 
-if __name__ == "__main__":
-    application = create_app()
-    application.run(debug=True)
+# if __name__ == "__main__":
+application = create_app()
+application.run(debug=True)
