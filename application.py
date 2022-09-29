6f1df270154735f581743f1bc2c83280cbb8a002
aclozovoy <aclozovoy@gmail.com>
2022-09-29 16:05:25 -0700

    Reorganize files

--- application.py
+++ application.py
@@ -1,5 +1,16 @@
-from website import create_app
+from flask import Flask
+
+def create_app():
+    application = Flask(__name__)
+    application.config['SECRET_KEY'] = "helloworld"
+
+    from views import views
+
+    application.register_blueprint(views, url_prefix="/")
+
+    return application
 
 # if __name__ == "__main__":
 application = create_app()
 application.run() #debug=True)
+
