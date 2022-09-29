586fc3e8b453f9a5eae5d3f3b420797638d1750b
aclozovoy <aclozovoy@gmail.com>
2022-09-29 16:30:41 -0700

    test

--- application.py
+++ application.py
@@ -1,20 +1,29 @@
 from flask import Flask
+from views import views
 
-def create_app():
-    application = Flask(__name__)
-    # application.config['SECRET_KEY'] = "helloworld"
+# def create_app():
+#     application = Flask(__name__)
+#     # application.config['SECRET_KEY'] = "helloworld"
 
-    from views import views
+#     from views import views
 
-    # application.register_blueprint(views, url_prefix="/")
+#     # application.register_blueprint(views, url_prefix="/")
 
-    return application
+#     return application
 
 # # if __name__ == "__main__":
 # application = create_app()
 # application.run() #debug=True)
 
 # application = Flask(__name__)
+
+
+application = Flask(__name__)
+# application.config['SECRET_KEY'] = "helloworld"
+# application.register_blueprint(views, url_prefix="/")
+application = create_app()
+application.run(debug=True)
+
 
 @application.route('/')
 def hello_world():
