81d18d5965decca005d3ebb5442749123a2b68f8
aclozovoy <aclozovoy@gmail.com>
2022-09-29 16:09:47 -0700

    Create simple test app

--- application.py
+++ application.py
@@ -1,16 +1,21 @@
 from flask import Flask
 
-def create_app():
-    application = Flask(__name__)
-    application.config['SECRET_KEY'] = "helloworld"
+# def create_app():
+#     application = Flask(__name__)
+#     application.config['SECRET_KEY'] = "helloworld"
 
-    from views import views
+#     from views import views
 
-    application.register_blueprint(views, url_prefix="/")
+#     application.register_blueprint(views, url_prefix="/")
 
-    return application
+#     return application
 
-# if __name__ == "__main__":
-application = create_app()
-application.run() #debug=True)
+# # if __name__ == "__main__":
+# application = create_app()
+# application.run() #debug=True)
 
+application = Flask(__name__)
+
+@application.route('/')
+def hello_world():
+    return 'Hola Mundo'
