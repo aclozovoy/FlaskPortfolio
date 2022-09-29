318d594260b644240640baafa27e31bea70cdd70
aclozovoy <aclozovoy@gmail.com>
2022-09-29 16:20:56 -0700

    remove key

--- application.py
+++ application.py
@@ -2,7 +2,7 @@ from flask import Flask
 
 def create_app():
     application = Flask(__name__)
-    application.config['SECRET_KEY'] = "helloworld"
+    # application.config['SECRET_KEY'] = "helloworld"
 
     from views import views
 
